c5d9284c59366b7402d539576aef982d96a4aba3
Marshall Chan <marshallchan666@outlook.com>
2022-01-07 04:35:02 +0800

    class and module

---
+++ 08typeTs/classTs.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-06 16:40:51
+ * @LastEditTime: 2022-01-06 17:07:44
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /diveIntoTs/08typeTs/classTs.ts
+ */
+
+// * abstract Class good for extends
---
+++ 08typeTs/moduleTs.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-06 17:12:49
+ * @LastEditTime: 2022-01-06 17:12:50
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /diveIntoTs/08typeTs/moduleTs.ts
+ */
